e1926b87c0e0fe5e60b7c61f2b3b73e0b3e69f10
DAVIDE SAVARRO <20023866@studenti.uniupo.it>
2022-07-15 20:35:12 +0200

    Fixed random walk model

--- randomWalkModel.py
+++ randomWalkModel.py
@@ -10,41 +10,51 @@ import networkx as nx
 from networkx.utils import powerlaw_sequence
 import scipy.stats as stats
 
-def buildRandomWalkModel(N, mMin, mMax, p):
-    nStart = 4
+def buildRandomWalkModel(N, m, p):
+    nStart = 2
     G = nx.complete_graph(nStart)
     for i in range(nStart, N):
-        newM = round(np.random.uniform(mMin, mMax))
         firstAttach = np.random.choice(G.nodes)
         neighIter = nx.neighbors(G, firstAttach)
         
         # Get the list of possible neighbors of "firstAttach"
-        possibleNeigh = []
+        originalNeigh = []
         for neigh in neighIter:
-            possibleNeigh.append(neigh)
+            originalNeigh.append(neigh)
         # Attach the new added node to the selected node
         G.add_edge(i, firstAttach)
         
-        if np.random.uniform() < p:
-            # Connect i to one of the neighbor of "firstAttach"
-            G.add_edge(i, np.random.choice(possibleNeigh))
-        else:
-            # Connect i to one of the other nodes    
-            nodes = set(G.nodes())
-            otherNodes = nodes.difference(set(possibleNeigh))
-            otherNodes = otherNodes.difference({i, firstAttach})
-            if(len(otherNodes) > 0) :
-                G.add_edge(i, np.random.choice(list(otherNodes)))
-    
+        possibleNeigh = originalNeigh
+        for j in range(0, m - 1):    
+            if np.random.uniform() < p:
+                # Connect i to one of the neighbor of "firstAttach"
+                if(len(possibleNeigh) > 0):
+                    print(possibleNeigh)
+                    choiceNeigh = np.random.choice(possibleNeigh)
+                    G.add_edge(i, choiceNeigh)
+                    possibleNeigh = list(set(possibleNeigh).difference({choiceNeigh}))
+            else: 
+                # Fine the neighors of i to exclude them
+                newNeigh = []
+                for neigh in nx.neighbors(G, i):
+                    newNeigh.append(neigh)
+
+                # Connect i to one of the other nodes
+                nodes = set(G.nodes())
+                otherNodes = nodes.difference(set(originalNeigh))
+                otherNodes = otherNodes.difference({i, firstAttach})
+                otherNodes = otherNodes.difference(set(newNeigh))
+                if(len(otherNodes) > 0) :
+                    G.add_edge(i, np.random.choice(list(otherNodes)))
+        
     print("Degree assortativity: " + nx.degree_assortativity_coefficient(G).__str__())
     print("Clustering coefficient: " + nx.average_clustering(G).__str__())
     nx.draw(G, with_labels=True)
     pylab.show()
     
-N = 10
-mMin = 2
-mMax = 6
+N = 100
+m = 8
 # Probability of triadic closure
-p = 0.8
+p = 1
 
-buildRandomWalkModel(N, mMin, mMax, p)
+buildRandomWalkModel(N, m, p)
